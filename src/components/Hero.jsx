2956cd88dfb449a024ac227966b663980d377011
jadenlohh <jadenlohh@gmail.com>
2024-03-11 18:24:02 +0800

    feat: add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Portrait from '../image/portrait.jpg';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/hero.css';
+
+const Hero = () => {
+  return (
+    <div className='container my-4'>
+      <div className='row'>
+        <div className='col-6'>
+          <div className='d-flex flex-column justify-content-center align-items-center h-100'>
+            <div>
+              <h1>Jaden Loh</h1>
+              <p>Year 3 student at Nanyang Polytechnic</p>
+
+              <button className='btn shadow-none mt-3 py-3 text-start w-100 d-flex justify-content-between'>
+                <span>Download Resume</span>
+                <span className='arrow-right'><i class="fa-solid fa-arrow-right"></i></span>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-6 text-center'>
+          <img className='img-fluid portrait' src={Portrait} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
