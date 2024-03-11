d346eea518cdbbf76ee24cdc44c6f2d913e57a0f
jadenlohh <jadenlohh@gmail.com>
2024-03-11 20:42:51 +0800

    feat: add about me component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AboutMe from './components/AboutMe';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
@@ -8,6 +9,7 @@ function App() {
       <div className='app'>
         <Navbar />
         <Hero />
+        <AboutMe />
       </div>
     </div>
   );
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/aboutMe.css';
+
+const AboutMe = () => {
+  return (
+    <div className='container my-4' id='about-me'>
+      <h3 className='mb-4'><span style={{color: '#666'}}>#</span>about-me</h3>
+      <p>
+        The first programming language that i learned through YouTube was Python when i was 16. Creating new programs has inspired me to 
+        pursure a Diploma in Information Technology where i got to build foundational knowledge in programming and developing solutions to
+        solve problems.
+      </p>
+      <p>
+        I inspire to use technology to solve real-world problems, building applications that makes an impact to the community.
+      </p>
+    </div>
+  )
+}
+
+export default AboutMe
