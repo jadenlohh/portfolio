143dc46ff2f16d4baf71d925f8cec474c49eed12
jadenlohh <jadenlohh@gmail.com>
2024-03-11 18:16:13 +0800

    chore: remove link for resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,10 @@ import '../styles/navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className='navbar justify-content-center pt-5'>
+    <nav className='navbar justify-content-center py-5'>
       <a className='mx-2 px-2' href='/'>Home</a>
       <a className='mx-2 px-2' href='#about-me'>About me</a>
       <a className='mx-2 px-2' href='#projects'>Projects</a>
-      <a className='mx-2 px-2' href='https://google.com' target='_blank' rel='noopener noreferrer '>Resume <span><i className='fa-solid fa-arrow-up-right-from-square ms-1'></i></span></a>
     </nav>
   )
 }
