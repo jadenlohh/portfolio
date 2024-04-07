ce8f0fbb0fa9e2f1095e46c6ed255b1ac481462a
jadenlohh <jadenlohh@gmail.com>
2024-04-07 22:18:12 +0800

    update about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,15 +9,18 @@ const AboutMe = () => {
         
         <div className='section-body'>
           <p>
-            The first programming language that i learned through YouTube was Python when i was 16. Creating new programs has inspired me to 
-            pursure a Diploma in Information Technology where i got to build foundational knowledge in programming and developing solutions to
-            solve problems.
+            The first programming language that i learned was Python when i was 16. Through YouTube videos and online research, i managed to build 
+            my first program and it inspired me to pursue a Diploma in Information Technology.
           </p>
           <p>
-            I inspire to use technology to solve real-world problems, building applications that makes an impact to the community.
+            Working on different government projects during my internship at NCS Group and brainstorming ideas for my school projects got me thinking - 
+            what if i used technology to create applications that solves real-world problems?
           </p>
           <p>
-            When I'm not at the computer, i'll usually be hanging out with friends and family, watching videos and running.
+            That is what i want to do now and in the future. To use my knowledge and experience in tech to make an impact on the community.
+          </p>
+          <p>
+            When I'm not at the computer, i'll usually be hanging out with my friends and family or running.
           </p>
         </div>
       </div>
