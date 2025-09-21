e01db4199c8a00ea7aed552c223ea0cca151559a
jadenlohh <jadenlohh@gmail.com>
2025-09-21 22:11:08 +0800

    style: add underline effect to navbar links when  hover

--- app/ui/navbar.jsx
+++ app/ui/navbar.jsx
@@ -1,19 +1,27 @@
-function navbar() {
+import React from "react";
+
+const navbar = () => {
   return (
     <div className="fixed top-0 w-full">
-      <nav className="flex justify-between items-center p-6">
+      <nav className="flex justify-between items-center p-7">
         <span className="text-2xl font-medium">Jaden.</span>
 
         <div>
           <ul>
-            <li className="inline px-4">Experience</li>
-            <li className="inline px-4">Projects</li>
-            <li className="inline px-4">Contact</li>
+            <li className="inline px-4">
+              <a href="" className="link link-underline link-underline-black p-0.5">Experience</a>
+            </li>
+            <li className="inline px-4">
+              <a href="" className="link link-underline link-underline-black p-0.5">Projects</a>
+            </li>
+            <li className="inline px-4">
+              <a href="" className="link link-underline link-underline-black p-0.5">Contact</a>
+            </li>
           </ul>
         </div>
       </nav>
     </div>
   );
-}
+};
 
 export default navbar;
