9a03fed5beef85ddb56053d31235786efaf81a71
jadenlohh <jadenlohh@gmail.com>
2025-09-21 22:05:09 +0800

    feat: add socials link to bottom of website

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "./ui/navbar";
+import Socials from "./ui/socials";
 
 export default function Home() {
   return (
     <div className="h-screen">
       <Navbar />
+      <Socials />
 
-      <div className="content-center w-full h-full px-6">
+      <div className="content-center w-full h-full px-7">
         <h1 className="text-5xl/14 font-medium">
           Graduate from Nanyang Polytechnic <br />
           Passionate about building tech for public good.
